53991b1314e6b143307042e20fbb6c4de32e6b9a
Furt <lit_69@hotmail.com>
2022-08-23 06:25:04 -0500

    Added utility files

---
+++ src/main/java/com/crimsonreborn/crimsonutils/utilities/config/exceptions/InvalidValueException.java
@@ -0,0 +1,10 @@
+package com.crimsonreborn.crimsonutils.utilities.config.exceptions;
+
+public class InvalidValueException extends Exception {
+
+    private static final long serialVersionUID = 7926546608790237325L;
+
+    public InvalidValueException(String msg) {
+        super(msg);
+    }
+}
